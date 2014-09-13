2ca54a784410df8698fc61c544bcc4e924aaefb2
Jacob Kahn <jacobkahn1@gmail.com>
2014-09-13 18:24:19 -0400

    New Javascript Parsers
    without implicit JSON functions

---
+++ entry_lookup.js
@@ -0,0 +1,22 @@
+
+calendarList = {"dictionary returned by basic calendar call to get email address"}
+
+
+
+function parse_calendar_name(calendarList)
+{
+	if (calendarList === )
+	json_full
+	return json_full
+}
+
+
+
+function parse_full_JSON_object(json_full){
+	for (i = 0; i < json.length; i++) 
+	{   
+		var event_name = json.items[i].summary
+		var start_time = json.items[i].start.datetime
+		var end_tie = json.item[i].end.datetime
+	}
+}
