5f632c3445d281db01d70660b210032d2ae8bad4
Devesh Dayal <devesh95@gmail.com>
2015-01-09 03:49:48 +0530

    Choose events to include in mapper

--- js/custom-functions.js
+++ js/custom-functions.js
@@ -1,3 +1,5 @@
+calendarData = new Array();
+
 function populateTime() {
 	var now = new Date();
 	var dayW = now.getDay();
@@ -38,6 +40,9 @@ function handleData(events) {
 	console.log("Final output:");
 	console.log(eventDetailsList);
 	populateTags(eventDetailsList);
+	for (i=0; i<eventDetailsList.length;  i++) {
+		calendarData.push(eventDetailsList[i])
+	}
 	if(eventDetailsList.length > 0) {
 		toggle_mainmapper(eventDetailsList);
 	}
@@ -47,29 +52,37 @@ function handleData(events) {
 	}
 }
 
+var eventnumber = 1; 
 function populateTags (details) {
-	m = 1; a = 1; e = 1; c1tag=""; c2tag=""; c3tag="";
+	var input = details;
+	c1tag=""; c2tag=""; c3tag="";
 	for(i = 0; i < details.length; i++) {
 		var time = details[i][3];
 		var name = details[i][1];
 		var check = parseInt(time.substring(0,2));
+		var tagname = eventnumber;
 		if(check < 8) {
-			var tagname = "m" + m;
-			misc = " AM"; m += 1;
-			c1tag = c1tag + "<li id=\""+tagname+"\"><a href=\"\">"+name+"</a><span id=\"timeStamp\">"+time+misc+"</span></li>";
+			misc = " AM";
+			c1tag = c1tag + "<li style=\"cursor: pointer\" onclick=\"toggleCheckBox(\'"+tagname+"\')\"><input type=\"checkbox\" id=\""+tagname+"\" style=\"float: left\" checked=\"checked\"><a>"+name+"</a><span id=\"timeStamp\">"+time+misc+"</span></li>";
 		}
 		else if((check >= 8) && (check < 16)) {
-			var tagname = "a" + a;
-			misc = " PM"; a += 1;
-			c2tag = c2tag + "<li id=\""+tagname+"\"><a href=\"\">"+name+"</a><span id=\"timeStamp\">"+time+misc+"</span></li>";
+			misc = " PM"; 
+			c2tag = c2tag + "<li style=\"cursor: pointer\" onclick=\"toggleCheckBox(\'"+tagname+"\', \'calendarData\')\"><input type=\"checkbox\" id=\""+tagname+"\" style=\"float: left\" checked=\"checked\"><a>"+name+"</a><span id=\"timeStamp\">"+time+misc+"</span></li>";
 		}
 		else if(check >= 16) {
-			var tagname = "e" + e;
-			misc = " PM"; e += 1;
-			c3tag = c3tag + "<li id=\""+tagname+"\"><a href=\"\">"+name+"</a><span id=\"timeStamp\">"+time+misc+"</span></li>";
+			misc = " PM"; 
+			c3tag = c3tag + "<li style=\"cursor: pointer\" onclick=\"toggleCheckBox(\'"+tagname+"\')\"><input type=\"checkbox\" id=\""+tagname+"\" style=\"float: left\" checked=\"checked\"><a>"+name+"</a><span id=\"timeStamp\">"+time+misc+"</span></li>";
 		} 
+		eventnumber += 1;
 	}
 	document.getElementById("c1").innerHTML = c1tag;
 	document.getElementById("c2").innerHTML = c2tag;
 	document.getElementById("c3").innerHTML = c3tag;
+}
+
+function toggleCheckBox(id, data) {
+	console.log("Toggling event tag: #"+id);
+	var toggle = document.getElementById(id).checked;
+	document.getElementById(id).checked = !toggle;
+	buildMap(calendarData);
 }
