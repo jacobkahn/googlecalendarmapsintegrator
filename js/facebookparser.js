62e1ec65cde29c38742d2783e00b7a91b61ec13d
Devesh Dayal <devesh95@gmail.com>
2015-01-17 13:43:48 -0500

    fixed

--- js/facebookparser.js
+++ js/facebookparser.js
@@ -4,13 +4,11 @@ function statusChangeCallback(response) {
     getEvents();
   } else if (response.status === 'not_authorized') {
     // The person is logged into Facebook, but not your app.
-    document.getElementById('status').innerHTML = 'Please log ' +
-      'into this app.';
+    console.log('Logged into Facebook but not into the app.');
   } else {
     // The person is not logged into Facebook, so we're not sure if
     // they are logged into this app or not.
-    document.getElementById('status').innerHTML = 'Please log ' +
-      'into Facebook.';
+    console.log('Not logged into Facebook.');
   }
 }
 
