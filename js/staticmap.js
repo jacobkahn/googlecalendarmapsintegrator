0376f8c7c9be9622e9245e5b7ecf9509c4468262
Devesh Dayal <devesh95@gmail.com>
2015-01-18 03:20:46 -0500

    removing prints

--- js/staticmap.js
+++ js/staticmap.js
@@ -11,7 +11,6 @@
         for (var i = 0; i < list_of_markers.length; i++)
         {
             st.markers.addGoogleMarker(list_of_markers[i]);
-            console.log(list_of_markers[i]);
         }
         // render route based on Google Maps 
         st.route.setRenderer(renderer);
